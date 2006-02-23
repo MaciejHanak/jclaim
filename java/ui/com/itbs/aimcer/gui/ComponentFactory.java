896368496767ebf62374865e70723c2300b0e368
alex_1 <alex_1@2da43bf6-6c0d-0410-a28d-f9ba0fb37c19>
2006-02-23 05:45:10 +0000

     Moved fixWidget here for convenience

--- java/ui/com/itbs/aimcer/gui/ComponentFactory.java
+++ java/ui/com/itbs/aimcer/gui/ComponentFactory.java
@@ -1,11 +1,15 @@
 package com.itbs.aimcer.gui;
 
+import com.itbs.aimcer.bean.ClientProperties;
+
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import javax.swing.plaf.metal.MetalBorders;
 import java.awt.*;
 
 /**
- * @author Created by Alex Rass on Sep 9, 2004
+ * @author Alex Rass
+ * @since Sep 9, 2004
  */
 public class ComponentFactory {
     public static JComponent getTopBar(String displayedText) {
@@ -16,5 +20,12 @@ public class ComponentFactory {
         label.setOpaque(true);
         label.setFont(label.getFont().deriveFont(16F));
         return label;
+    }
+
+
+    public static void fixWidget(final JComponent comp, String title) {
+        comp.setFont(new Font("Arial", Font.PLAIN, ClientProperties.INSTANCE.getFontSize()+1));
+        comp.setOpaque(false);
+        comp.setBorder(new TitledBorder(comp.getBorder(), title, TitledBorder.CENTER, TitledBorder.ABOVE_TOP));
     }
 }
