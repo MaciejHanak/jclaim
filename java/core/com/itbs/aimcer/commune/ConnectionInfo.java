d0c4b2a4adfeb22ff78ec4209852e1facedf68c8
alex_1 <alex_1@2da43bf6-6c0d-0410-a28d-f9ba0fb37c19>
2006-05-22 14:46:49 +0000

    Added support for Strings in host.  Added support for persistance.

--- java/core/com/itbs/aimcer/commune/ConnectionInfo.java
+++ java/core/com/itbs/aimcer/commune/ConnectionInfo.java
@@ -21,9 +21,10 @@
 package com.itbs.aimcer.commune;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
- * Used in file transfers.
+ * Used in file transfers and other things requiring an IP and port.
  * 
  * @author Alex Rass
  * @since Dec 19, 2004
@@ -31,16 +32,60 @@ import java.net.InetAddress;
 public class ConnectionInfo {
     InetAddress ip;
     private int port;
+
+    /**
+     * Do not use this constructor unless you set the things youself!
+     * @deprecated This is for XML persistance.  Do not use!
+     */
+    public ConnectionInfo() {
+    }
+
+    /**
+     *  Constructor.
+     *
+     * @param ip of the thing
+     * @param port of the thing
+     * @throws UnknownHostException when host information is wrong, see this for details: {@link InetAddress#getByName(String)}
+     * @see InetAddress
+     */
+    public ConnectionInfo(String ip, int port) throws UnknownHostException {
+        this(InetAddress.getByName(ip), port);
+    }
+
+    /**
+     * Constructor
+     * @param ip of the thing
+     * @param port of the thing
+     * @see InetAddress
+     */
     public ConnectionInfo(InetAddress ip, int port) {
-        this.ip = ip;
-        this.port = port;
+        setIp(ip);
+        setPort(port);
     }
 
     public InetAddress getIp() {
         return ip;
     }
 
+    public void setIp(InetAddress ip) {
+        if (ip == null) throw new NullPointerException("IP can not be null");
+        this.ip = ip;
+    }
+
+    public void setPort(int port) {
+        if (port < 1) throw new IllegalArgumentException("Port must be a number greater than 0");
+        this.port = port;
+    }
+
     public int getPort() {
         return port;
+    }
+
+    public boolean equals(Object obj) {
+        return obj instanceof ConnectionInfo && ((ConnectionInfo) obj).port == port && ((ConnectionInfo) obj).ip.equals(ip);
+    }
+
+    public String toString() {
+        return super.toString() + "[" + ip + ":" + port + "]";
     }
 }
