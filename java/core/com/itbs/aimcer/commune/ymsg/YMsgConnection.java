26347cd8f76b1363bc968423bf1be8d1b864d425
alex_1 <alex_1@2da43bf6-6c0d-0410-a28d-f9ba0fb37c19>
2006-07-18 02:03:40 +0000

    Fixed the problem with deleting contacts from last group.

--- java/core/com/itbs/aimcer/commune/ymsg/YMsgConnection.java
+++ java/core/com/itbs/aimcer/commune/ymsg/YMsgConnection.java
@@ -222,7 +222,7 @@ public class YMsgConnection extends AbstractMessageConnection {//implements File
     public void removeContact(Nameable contact) {
         // do it for the server
         GroupList list = getGroupList();
-        for (int i = list.size(); i>0; i--) {
+        for (int i = list.size()-1; i >= 0; i--) {
             try {
                 if (list.get(i).remove(contact)) {
                     session.removeFriend(contact.getName(), list.get(i).getName());
