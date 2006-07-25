972196c562a600c4e0e6f1254d6152341cc00928
alex_1 <alex_1@2da43bf6-6c0d-0410-a28d-f9ba0fb37c19>
2006-07-25 12:11:13 +0000

    Extracted some stuff to a Base class

--- java/ui/com/itbs/aimcer/gui/MessageGroupWindow.java
+++ java/ui/com/itbs/aimcer/gui/MessageGroupWindow.java
@@ -28,16 +28,14 @@ import com.itbs.aimcer.commune.MessageSupport;
 import com.itbs.gui.*;
 
 import javax.swing.*;
-import javax.swing.text.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.StyleConstants;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -47,23 +45,11 @@ import java.util.logging.Logger;
  * @author Alex Rass 
  * @since Sep 9, 2004
  */
-public class MessageGroupWindow  {
+public class MessageGroupWindow  extends MessageWindowBase {
     private static final Logger log = Logger.getLogger(MessageGroupWindow.class.getName());
-    /** Size of the message box. */
-    public static final Rectangle DEFAULT_SIZE = new Rectangle(420, 200, 350, 330);
-    /** Where the line is. */
-    private static final double DEFAULT_SEPARATION = 3.0 / 5.0;//150;
 
     /** Target Audience */
     private ContactWrapper[] contactWrapper;
-    /** message frame. */
-    JFrame frame;
-
-    MutableAttributeSet ATT_NORMAL, ATT_RED, ATT_BLUE, ATT_GRAY;
-
-    private BetterTextPane textPane;
-    private JTextPane historyPane;
-    private final AbstractAction ACTION_SEND;
 
     /**
      * Constructor
@@ -102,21 +88,7 @@ public class MessageGroupWindow  {
                                 textPane.requestFocus();
                             }
                         }, 'S');
-        frame.getContentPane().setLayout(new BorderLayout());
-        frame.getContentPane().add(getComponents());
-        frame.getContentPane().add(getButtons(), BorderLayout.SOUTH);
-        frame.setVisible(true);
-        SwingUtilities.invokeLater(new Runnable(){
-            public void run() {
-                textPane.requestFocus();
-            }
-        });
-        try {
-            if (ClientProperties.INSTANCE.isSpellCheck())
-                JazzyInterface.create().addSpellCheckComponent(textPane);
-        } catch (IOException e) {
-            log.log(Level.SEVERE, "", e);
-        }
+        composeUI();
     }
 
     /**
@@ -129,7 +101,7 @@ public class MessageGroupWindow  {
         return new MessageGroupWindow(wrappers);
     }
 
-    private Component getButtons() {
+    protected Component getButtons() {
         JPanel south = new JPanel();
         south.setLayout(new BoxLayout(south, BoxLayout.Y_AXIS));
         JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 3, 3));
@@ -140,33 +112,7 @@ public class MessageGroupWindow  {
         return south;
     }
 
-    /**
-     * Adds history pane and typing space.
-     * @return panel with components
-     */
-    private Component getComponents() {
-        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, getHistory(), getMessage());
-        splitPane.setDividerLocation((int)(frame.getHeight() * DEFAULT_SEPARATION));
-        return splitPane;
-    }
-
-    /**
-     * Created the typing window.
-     * @return panel with typing space.
-     */
-    private Component getMessage() {
-        textPane = new BetterTextPane(ACTION_SEND);
-//        textPane.setContentType("text/html");
-        if (!ClientProperties.INSTANCE.isEnterSends()) {
-            textPane.addModifier(KeyEvent.SHIFT_DOWN_MASK);
-            textPane.addModifier(KeyEvent.CTRL_DOWN_MASK);
-        }
-        JPanel typingSpace = new JPanel(new BorderLayout());
-        typingSpace.add(new JScrollPane(textPane, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
-        return typingSpace;
-    }
-
-    private Component getHistory() {
+    protected Component getHistory() {
 //        historyPane = new JTextPane();
         historyPane = new BetterTextPane();
         historyPane.setEditable(false);
@@ -192,37 +138,6 @@ public class MessageGroupWindow  {
         return jScrollPane;
     }
 
-    private void recalculateAttributes() {
-        ATT_NORMAL = new SimpleAttributeSet();
-        StyleConstants.setFontFamily(ATT_NORMAL,"Monospaced");
-        StyleConstants.setFontSize(ATT_NORMAL, historyPane.getFont().getSize());
-        ATT_BLUE = (MutableAttributeSet) ATT_NORMAL.copyAttributes();
-        ATT_RED = (MutableAttributeSet) ATT_NORMAL.copyAttributes();
-        ATT_GRAY = (MutableAttributeSet) ATT_NORMAL.copyAttributes();
-        StyleConstants.setForeground(ATT_BLUE, Color.BLUE);
-        StyleConstants.setForeground(ATT_RED,  Color.RED);
-        StyleConstants.setForeground(ATT_GRAY, Color.GRAY);
-    }
-
-    /** Helper. */
-    void appendHistoryText(final String text) {
-        appendHistoryText(text, ATT_NORMAL);
-    }
-
-    void appendHistoryText(final String text, final AttributeSet style) {
-        GUIUtils.runOnAWT(new Runnable() {
-            public void run() {
-//                historyPane.setText(historyPane.getText() + text);
-                final Document document = historyPane.getDocument();
-                try {
-                    document.insertString(document.getLength(), text, style);
-                } catch (BadLocationException e) {
-                    ErrorDialog.displayError(frame, "Failed to display proper text:\n\n"+ text, e);
-                }
-                historyPane.setCaretPosition(historyPane.getDocument().getLength());
-            }
-        });
-    }
 
     private void appendHistoryText(final String prefix, final ContactWrapper sendTo, final String text) {
         GUIUtils.runOnAWT(new Runnable() {
@@ -242,26 +157,12 @@ public class MessageGroupWindow  {
             }
         });
     }
-
-    DateFormat timeFormat = new SimpleDateFormat("<hh:mm>");
     public void addTextToHistoryPanel(Message message) throws IOException {
-/*
-        ContactWrapper contactWrapper = ((ContactWrapper)message.getContact());
-        if (ClientProperties.INSTANCE.getDisclaimerMessage().trim().length() > 0 && (contactWrapper.getLastDisclaimerTime() == 0 ||
-                System.currentTimeMillis() - contactWrapper.getLastDisclaimerTime() > ClientProperties.INSTANCE.getDisclaimerInterval())) {
-            contactWrapper.setLastDisclaimerTime();
-            Message disclMessage = new MessageImpl(contactWrapper, true, false, ClientProperties.INSTANCE.getDisclaimerMessage());
-            contactWrapper.getConnection().sendMessage(disclMessage);
-            // take care of the disclaimer!
-            //Main.getLogger().log(disclMessage);
-            appendText(ClientProperties.INSTANCE.getDisclaimerMessage());
-        }
-*/
         try {
             // now the actual message
 //            Message messageOut = new MessageImpl(contactWrapper, toBuddy, message);
             //Main.getLogger().log(messageOut);
-            appendHistoryText((ClientProperties.INSTANCE.isShowTime() ? timeFormat.format(new Date()) : ""),
+            appendHistoryText((ClientProperties.INSTANCE.isShowTime() ? TIME_FORMAT.format(new Date()) : ""),
                     (ContactWrapper) message.getContact(), (message.isAutoResponse()?"Automatic response: ":"") + message.getPlainText());
             Toolkit.getDefaultToolkit().beep();
         } catch (Exception e) {
