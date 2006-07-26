7d53657d4c8c032aa2fe5d0dc81a789087826d4d
alex_1 <alex_1@2da43bf6-6c0d-0410-a28d-f9ba0fb37c19>
2006-07-26 02:16:14 +0000

    Now extends connection

--- java/core/com/itbs/aimcer/commune/ChatRoomSupport.java
+++ java/core/com/itbs/aimcer/commune/ChatRoomSupport.java
@@ -2,11 +2,11 @@ package com.itbs.aimcer.commune;
 
 /**
  * Methods which are required to be implemented for Group Chat support.
- * 
+ *
  * @author Alex Rass
  * @since Oct 10, 2005
  */
-public interface ChatRoomSupport {
+public interface ChatRoomSupport extends Connection {
     void join(String room, String nickname, ChatRoomEventListener listener);
     void create(String room, String nickname, ChatRoomEventListener listener);
     void sendChatMessage(String message);
