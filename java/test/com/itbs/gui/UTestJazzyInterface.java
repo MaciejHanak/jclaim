a96e8c022dcb9f900d89d560b4ff55775908b013
alex_1 <alex_1@2da43bf6-6c0d-0410-a28d-f9ba0fb37c19>
2006-09-18 05:40:19 +0000

    No longer complains about window parent.

--- java/test/com/itbs/gui/UTestJazzyInterface.java
+++ java/test/com/itbs/gui/UTestJazzyInterface.java
@@ -23,6 +23,7 @@ package com.itbs.gui;
 import com.itbs.aimcer.gui.UTestFrameTest;
 
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 import java.awt.*;
 
 /**
@@ -30,16 +31,16 @@ import java.awt.*;
  * @since Nov 7, 2004
  */
 public class UTestJazzyInterface extends UTestFrameTest {
-    JTextArea comp = new JTextArea(5, 20);
+    JTextComponent comp = new BetterTextPane();//5, 20);
 
 
 
 
     public void testMain() throws Exception {
         comp.setText("This is the story\nof the hare who\nlost his spectacles.");
-        JazzyInterface.create().addSpellCheckComponent(comp);
 
         window.add(new JScrollPane(comp), BorderLayout.CENTER);
+        JazzyInterface.create().addSpellCheckComponent(comp);
 
         window.setSize(500, 400);
         window.setVisible(true);
