547322e3bfd6b5c07aba382cb9853089e212a047
alex_1 <alex_1@2da43bf6-6c0d-0410-a28d-f9ba0fb37c19>
2007-05-22 17:33:55 +0000

    Moved the new code out of here, since it doen't really fit the architecture of this singleton.  (it's very connection and UI specific).  Moved to MessageWindow.

--- java/core/com/itbs/aimcer/commune/SMS/SMSWrapper.java
+++ java/core/com/itbs/aimcer/commune/SMS/SMSWrapper.java
@@ -20,16 +20,14 @@
 
 package com.itbs.aimcer.commune.SMS;
 
-import com.itbs.aimcer.bean.ContactWrapper;
-import com.itbs.aimcer.commune.Connection;
-import com.itbs.aimcer.commune.SMSSupport;
-import com.itbs.aimcer.gui.MessageWindow;
 import com.itbs.util.GeneralUtils;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
+ * Obsolete.  All the websites are closing this down.
+ * <b>Use the IM connections which follor SMSSupport interface.</b>
+ *
  * @author Alex Rass on May 28, 2005
  */
 public class SMSWrapper {
@@ -38,55 +36,6 @@ public class SMSWrapper {
     public static final SMSMessage TMOBILE = new TMobile();
     public static final SMSMessage CINGULAR = new Cingular();
     public static final SMSMessage VERISON = new Verizon();
-
-    /**
-     * Sends message via any available media.
-     * @param conns All available connections to try (can be null)
-     * @param conn Preferred connection to first use. Used to get From preferences. (can not be null)
-     * @param to whom
-     * @throws InvalidDataException when number is wrong
-     */
-    public static void sendMessage(List<Connection> conns, Connection conn, ContactWrapper to) throws InvalidDataException {
-        if (GeneralUtils.isNotEmpty(conn.getUser().getName()) && GeneralUtils.isNotEmpty(to.getPreferences().getPhone())) {
-            String result="";
-            boolean found = false;
-            if (conn instanceof SMSSupport && conn.isLoggedIn()) {
-                result = ((SMSSupport)conn).veryfySupport(to.getPreferences().getPhone());
-                if (result == null) {
-                    found = true;
-                    ContactWrapper cw = ContactWrapper.create(to.getPreferences().getPhone(), conn);
-                    cw.getPreferences().setName(to.getPreferences().getName());
-                    cw.getPreferences().setDisplayName(to.getPreferences().getDisplayName() + " (Phone)");
-                    MessageWindow.openWindow(cw, true);
-                }
-            }
-            if (!found && conns!=null) { // Search
-                String tempResult;
-                for (Connection connection : conns) {
-                    if (connection instanceof SMSSupport){
-                        if (!connection.isLoggedIn()) {
-                            result += "Connection " + connection.getUser().getName() + " on " + connection.getServiceName() + " can't be used (not logged in).\n";
-                            continue;
-                        }
-                        tempResult = ((SMSSupport)connection).veryfySupport(to.getPreferences().getPhone());
-                        if (tempResult!=null) {
-                            result += tempResult + "\n";
-                            continue;
-                        }
-                        found = true;
-                        ContactWrapper cw = ContactWrapper.create(to.getPreferences().getPhone(), connection);
-                        cw.getPreferences().setName(to.getPreferences().getName());
-                        cw.getPreferences().setDisplayName(to.getPreferences().getDisplayName() + " (Phone)");
-                        MessageWindow.openWindow(cw, true);
-                    }
-                }
-                if (!found) {
-                    throw new InvalidDataException("Failed to send for any one of the following reasons:\n" + result);
-                }
-            }
-        }
-        throw new InvalidDataException("Missing required parameter.");
-    }
 
     /**
      * Sends message via any available media.
