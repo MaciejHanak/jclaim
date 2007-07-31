633149c2d9ffb1001ffab1e3b5a5bf6866678987
alex_1 <alex_1@2da43bf6-6c0d-0410-a28d-f9ba0fb37c19>
2007-07-31 17:15:16 +0000

    Javadoc

--- java/ui/com/itbs/aimcer/web/HelpServlet.java
+++ java/ui/com/itbs/aimcer/web/HelpServlet.java
@@ -69,7 +69,7 @@ public class HelpServlet extends HttpServlet {
      * Services a single HTTP request from the client.
      *
      * @param req the HTTP request
-     * @param req the HTTP response
+     * @param res the HTTP response
      * @throws javax.servlet.ServletException
      *                             when a Servlet exception has occurred
      * @throws java.io.IOException when an I/O exception has occurred
