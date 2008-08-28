68adee4b93cf5f701c2b86b1cd0e732e9f4908b4
alex_1 <alex_1@2da43bf6-6c0d-0410-a28d-f9ba0fb37c19>
2008-08-28 06:19:48 +0000

    Moved a helper class out to gui utils package.

--- java/ui/com/itbs/aimcer/gui/PropertiesDialog.java
+++ java/ui/com/itbs/aimcer/gui/PropertiesDialog.java
@@ -29,8 +29,6 @@ import com.itbs.gui.*;
 import com.itbs.util.SoundHelper;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import javax.swing.text.JTextComponent;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -513,35 +511,5 @@ final public class PropertiesDialog extends JDialog implements ActionListener {
         }
         ServerStarter.update();
         Main.getPeoplePanel().update();
-    }
-
-
-    /**
-     * Allows to easily disable all other buttons.
-     */
-    private static class TurnOffDependents implements ActionListener, ChangeListener {
-        JComponent[] dependents;
-
-        /**
-         * Constructor. 
-         * @param components list of components in the group.
-         */
-        public TurnOffDependents(JComponent[] components) {
-            dependents = components;
-        }
-
-        public void stateChanged(ChangeEvent e) {
-            if (e.getSource() instanceof AbstractButton)
-                for (JComponent dependent : dependents) {
-                    dependent.setEnabled(((AbstractButton) e.getSource()).isSelected());
-                }
-        }
-
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() instanceof AbstractButton)
-                for (JComponent dependent : dependents) {
-                    dependent.setEnabled(((AbstractButton) e.getSource()).isSelected());
-                }
-        }
     }
 }
